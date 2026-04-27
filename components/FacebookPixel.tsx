cc90eee803dbb1237201f928bf5f5019662da3ea
Nissim Boaron <nsn770@gmail.com>
2026-04-28 00:35:53 +0300

    Fix Meta Pixel: load immediately with afterInteractive

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -1,30 +1,21 @@
-'use client';
-import { useEffect } from 'react';
+import Script from 'next/script';
+
+const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
 export function FacebookPixel() {
-  useEffect(() => {
-    const load = () => {
-      if ((window as any).fbq) return;
-      const s = document.createElement('script');
-      s.src = 'https://connect.facebook.net/en_US/fbevents.js';
-      s.async = true;
-      document.head.appendChild(s);
-      s.onload = () => {
-        (window as any).fbq('init', process.env.NEXT_PUBLIC_META_PIXEL_ID);
-        (window as any).fbq('track', 'PageView');
-      };
-      window.removeEventListener('scroll', load);
-      window.removeEventListener('click', load);
-      window.removeEventListener('touchstart', load);
-    };
-    window.addEventListener('scroll', load, { once: true });
-    window.addEventListener('click', load, { once: true });
-    window.addEventListener('touchstart', load, { once: true });
-    return () => {
-      window.removeEventListener('scroll', load);
-      window.removeEventListener('click', load);
-      window.removeEventListener('touchstart', load);
-    };
-  }, []);
-  return null;
+  if (!PIXEL_ID) return null;
+  return (
+    <Script id="meta-pixel" strategy="afterInteractive">{`
+      !function(f,b,e,v,n,t,s){
+      if(f.fbq)return;n=f.fbq=function(){
+      n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init','${PIXEL_ID}');
+      fbq('track','PageView');
+    `}</Script>
+  );
 }
