fd798c72c73f97260588506d0820e80ba93ef76f
Nissim Boaron <nsn770@gmail.com>
2026-04-28 11:48:59 +0300

    perf: optimize both hero banner images with Cloudinary transformations

--- app/components/SmartHero.tsx
+++ app/components/SmartHero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const SLIDES = [
   {
-    src: 'https://res.cloudinary.com/dyxzq3ucy/image/upload/w_1200,q_auto:good,f_auto/v1777365002/%D7%94%D7%99%D7%97%D7%99%D7%93%D7%99%D7%9D_%D7%91%D7%A2%D7%95%D7%9C%D7%9D_%D7%A9%D7%9E%D7%A8%D7%90%D7%99%D7%9D_%D7%9C%D7%9A_%D7%90%D7%AA_%D7%94%D7%A1%D7%95%D7%A4%D7%A8._%D7%A1%D7%AA_%D7%9E_%D7%95%D7%99%D7%95%D7%93%D7%90%D7%99%D7%A7%D7%94_%D7%9E%D7%94%D7%95%D7%93%D7%A8%D7%99%D7%9D_%D7%91%D7%A9%D7%A7%D7%99%D7%A4%D7%95%D7%AA_%D7%A9%D7%A2%D7%95%D7%93_%D7%9C%D7%90_%D7%94%D7%9B%D7%A8%D7%AA._%D7%94%D7%9E%D7%96%D7%95%D7%96%D7%95%D7%AA_%D7%A0%D7%91%D7%93%D7%A7%D7%95_%D7%A2%D7%99_%D7%A8%D7%91_%D7%9E%D7%95%D7%A1%D7%9E%D7%9A_%D7%A6%D7%99%D7%9C%D7%95%D7%9D_%D7%A7%D7%9C%D7%A3_%D7%90%D7%9E%D7%99%D7%AA%D7%99_%D7%9C%D7%A4%D7%A0%D7%99_%D7%9E%D7%A9%D7%9C%D7%95%D7%97_%D7%AA%D7%A2%D7%95%D7%93%D7%AA_%D7%9B%D7%A9%D7%A8%D7%95%D7%AA_%D7%95%D7%94%D7%A1%D7%9E%D7%9B%D7%94_%D7%9C%D7%9B%D7%9C_%D7%A1%D7%95%D7%A4%D7%A8_hiascz.png',
+    src: 'https://res.cloudinary.com/dyxzq3ucy/image/upload/w_1200,q_auto:good,f_auto/v1777365682/%D7%91%D7%90%D7%A0%D7%A8_2_wovsve.png',
     alt: 'Your Sofer — סת״מ ויודאיקה מהודרים',
     priority: true,
   },
