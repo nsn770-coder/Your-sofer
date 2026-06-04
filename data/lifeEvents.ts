5713dee6a1a6b903f95c20ad71ba926759c4c26b
Nissim <nsn770@gmail.com>
2026-06-04 18:20:36 +0300

    feat: update wedding card image to הפרשת חלה photo

--- data/lifeEvents.ts
+++ data/lifeEvents.ts
@@ -28,7 +28,7 @@ export const lifeEvents: LifeEvent[] = [
     title: 'חתונה והפרשת חלה',
     emotionalTitle: 'תחילת הבית היהודי',
     description: 'מתנות לחתן וכלה, סטים מיוחדים, תכשיטים ועיצוב לבית החדש שמתחיל.',
-    image: 'https://res.cloudinary.com/dyxzq3ucy/image/upload/v1780390726/%D7%97%D7%AA%D7%95%D7%A0%D7%94_%D7%9E%D7%A9%D7%A4%D7%97%D7%94_kg2ifa.png',
+    image: 'https://res.cloudinary.com/dyxzq3ucy/image/upload/v1780586286/%D7%94%D7%A4%D7%A8%D7%A9%D7%AA_%D7%97%D7%9C%D7%94_1_badjmr.png',
     relatedCategories: [
       { category: 'סט טלית תפילין',  subCategories: 'all' },
       { category: 'טליתות וציציות',   subCategories: 'all' },
